0751c42bf510f0fc61429015978a3ca58df9f935
IKhlash Mulyanurahman <nurahmanmulya@gmail.com>
2023-06-09 12:39:28 +0700

    add environment configuration

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"os"
+
+	"github.com/ikhlashmulya/noteapp-resful-api/exception"
+	"github.com/joho/godotenv"
+)
+
+//configuration .env
+
+type Config interface {
+	Get(key string) string
+}
+
+type ConfigImpl struct {
+}
+
+func (config *ConfigImpl) Get(key string) string {
+	return os.Getenv(key)
+}
+
+func NewConfig(fileNames ...string) Config {
+	err := godotenv.Load(fileNames...)
+	exception.PanicIfErr(err)
+
+	return &ConfigImpl{}
+}
